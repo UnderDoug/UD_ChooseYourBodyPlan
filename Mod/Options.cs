e610cde86743cf916537c14bede74c73571c784a
UnderDoug <douglas.marken@gmail.com>
2026-02-28 22:55:28 +1000

    Added Options.

---
+++ Mod/Options.cs
@@ -0,0 +1,21 @@
+﻿using XRL;
+
+namespace UD_BodyPlan_Selection.Mod
+{
+    [HasOptionFlagUpdate(Prefix = "Option_UD_BodyPlan_Selection_", FieldFlags = true)]
+    public static class Options
+    {
+        // Debug Settings
+        // public static bool DebugEnableOption;
+
+        // General Settings
+        public static bool EnableBodyPlansForTK;
+
+        public static bool EnableBodyPlansAvailableViaRecipe;
+
+        public static bool EnableBodyPlansThatAreRobotic;
+        public static bool EnableBodyPlansThatAreRoboticWithoutMakingYouRobotic;
+
+        public static bool EnableBodyPlansThatSuck;
+    }
+}
